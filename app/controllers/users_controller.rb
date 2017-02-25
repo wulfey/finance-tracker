56fa2f9ecf7ce97b20edee2931368386eee51cbb
Jason Wolfe <jasonwolfe@gmail.com>
2017-02-25 06:04:06 +0000

    friendship model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,9 @@ class UsersController < ApplicationController
         @user = current_user
     end
     
+    def my_friends
+        
+    end
+    
 end
  
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ActiveRecord::Base
   has_many :user_stocks 
   has_many :stocks, through: :user_stocks
   
+  has_many :friendships
+  has_many :friends, through: :friendships
+  
   
   def can_add_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_added?(ticker_symbol)
@@ -20,6 +23,11 @@ class User < ActiveRecord::Base
     stock = Stock.find_by_ticker(ticker_symbol)
     return false unless stock 
     user_stocks.where(stock_id: stock.id).exists?
+  end
+  
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonymous"
   end
   
 end
