2d052f3576ecaf24c763cca6b884e8840675bb69
Jason Wolfe <jasonwolfe@gmail.com>
2017-02-26 05:41:25 +0000

    add destroy and view friend stock functions

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,13 @@
+class FriendshipsController < ApplicationController
+
+    
+    
+    def destroy 
+        @friendship = current_user.friendships.where(friend_id: params[:id]).first
+        @friendship.destroy 
+        respond_to do |format|
+            format.html { redirect_to my_friends_path, notice: "Friend was successfuly removed." }
+        end
+    end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,10 @@ class UsersController < ApplicationController
         end
     end
     
+    def show 
+        @user = User.find(params[:id])
+        @user_stocks = @user.stocks
+    end
 
     
 end
